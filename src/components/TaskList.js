672c604276fbdc05a0679a44dff7d959b62a884e
EMH <morfinworking@gmail.com>
2022-09-27 17:36:47 -0500

    Autoformat

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -11,18 +11,18 @@ const LoadingTask = () => (
       borderBottom: '1px',
       borderColor: 'gray.200',
     }}
-    bg="white"
-    alignItems="center"
+    bg='black'
+    alignItems='center'
     h={12}
     p={4}
-    aria-busy="true"
+    aria-busy='true'
   >
     <Skeleton height={4} width={4} mr={4} />
     <Skeleton height={4} width={40} mr={2} />
     <Skeleton height={4} width={6} mr={2} />
     <Skeleton height={4} width={12} />
     <Spacer />
-    <StarIcon aria-hidden="true" height={4} width={4} color="gray.200" />
+    <StarIcon aria-hidden='true' height={4} width={4} color='gray.200' />
   </Flex>
 );
 
@@ -41,7 +41,7 @@ export function TaskList({
 
   if (loading) {
     return (
-      <VStack align="stretch" spacing={0}>
+      <VStack align='stretch' spacing={0}>
         <LoadingTask />
         <LoadingTask />
         <LoadingTask />
@@ -54,8 +54,8 @@ export function TaskList({
       <EmptyState
         minHeight={72}
         Icon={CheckIcon}
-        title="You have no tasks"
-        subtitle="Sit back and relax"
+        title='You have no tasks'
+        subtitle='Sit back and relax'
       />
     );
   }
@@ -66,7 +66,7 @@ export function TaskList({
   ];
 
   return (
-    <VStack as="ul" align="stretch" spacing={0} aria-label="tasks">
+    <VStack as='ul' align='stretch' spacing={0} aria-label='tasks'>
       {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
